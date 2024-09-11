8e1692e7c393fad86fdf70dd34597f02728b1a00
aaron21913 <atngo106@student.hbuhsd.edu>
2024-09-11 10:50:39 -0700

    done lol

--- Problem2.java
+++ Problem2.java
@@ -2,7 +2,15 @@ public class Problem2
 {
   public static void main(String[] args)
   {
-    int rizzThree = 123;
-    System.out.println("Three number dab number" + rizzThree);
+    final int num = 456;
+    int first = num/100;
+    int last = num % 10;
+    int second = (num - (first*100) - last)/10;
+
+    System.out.println("Three digit number is " + num);
+    System.out.println(first);
+    System.out.println(second);
+    System.out.println(last);
+
   }
 }
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,15 @@
 
+public class Problem3
+{
+    public static void main(String[] args)
+    {
+        int rizzThree = 1234;
+    System.out.println(rizzThree % 10);
+    rizzThree /= 10;
+    System.out.println(rizzThree % 10);
+    rizzThree /= 10;
+    System.out.println(rizzThree % 10);
+    rizzThree /= 10;
+    System.out.println(rizzThree % 10);
+    }
+}
